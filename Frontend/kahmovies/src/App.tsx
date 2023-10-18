c4ce7419022452cc1ce31a463bf60e20ed32aedb
KauanMP <kauanmoreira1232@gmail.com>
2023-10-17 23:48:40 -0300

    Feito: Estrutura e css da navbar e do footer e gerenciamento de rotas na app

--- Frontend/kahmovies/src/App.tsx
+++ Frontend/kahmovies/src/App.tsx
@@ -1,11 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Header/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./Pages/Home/Home";
 
 function App() {
-
   return (
     <div>
-      <h1>KahMovies</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" Component={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/kahmovies/src/components/Header/Navbar.tsx
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <header id="nav">
+      <div>
+        <h2>KahMovies</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
